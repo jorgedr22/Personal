bdfcc6391b6c364c453cd971af3fc17656178abb
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2023-03-16 16:56:55 -0700

    .

--- PP_1_tb.v
+++ PP_1_tb.v
@@ -6,9 +6,9 @@ reg x,y,Rst,Clk;
 wire q;
 
 always begin
-        Clk <= 0;
+        Clk = 0;
         #100;
-        Clk <= 1;
+        Clk = 1;
         #100;
     end
 
@@ -18,16 +18,16 @@ initial begin
     $dumpfile("PP_1_tb.vcd");
     $dumpvars(0,PP_1_tb);
 
-    Rst<=1;x<=0;y<=0;
-    //traverse SA->SB->SC->SD
+    Rst=1;x=0;y=0;
+
     @(posedge Clk);
-    #50 Rst<=0;x<=1;y<=0;
+    #50 Rst=0;x=1;y=0;
     
     @(posedge Clk);
-    #50 x<=0;y<=1;
+    #50 x=0;y=1;
     
     @(posedge Clk); 
-    #50 x<=0;y<=0;
+    #50 x=0;y=0;
 
     $display("DONE");
 end
