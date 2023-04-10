f9294ca24bf84fcb94442a7987a210edbf153dc5
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2023-04-09 23:49:25 -0700

    .

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -3,52 +3,53 @@ import os
 import sys
 from collections import OrderedDict
 import time
-import fireworks
+import fireworks  
 import pygame
 pygame.init()
-def start_game(mystery_word,op):
-    print("The word is:\n")
-    print('_ _ _ _ _')
 
-    Op = 6*op;
-    for i in mystery_word:
-        print(mystery_word[i],end="")
-    print("")
+def start_game(mystery_word,op):
+     print("The word is:\n")
+     print('_ _ _ _ _')
+
+     Op = 6*op;
+     for i in mystery_word:
+         print(mystery_word[i],end="")
+     print("")
     
-    string_letters = "_____"
-    trys=10;
-    count=0
+     string_letters = "_____"
+     trys=10;
+     count=0
     
-    while trys>0:
-        inp = input("Guess a letter:")
+     while trys>0:
+         inp = input("Guess a letter:")
     
-        if inp in mystery_word.values():
-            os.system("clear")
-            for j in range(Op,Op+5):
-                if inp == mystery_word[j]:
-                    string_letters = string_letters[:(j%6)] + inp + string_letters[((j%6)+1):]
-                    count+=1                
-        else:
-            trys -= 1
-            print("Life lost! Lives left:"+str(trys)+"\n")
+         if inp in mystery_word.values():
+             os.system("clear")
+             for j in range(Op,Op+5):
+                 if inp == mystery_word[j]:
+                     string_letters = string_letters[:(j%6)] + inp + string_letters[((j%6)+1):]
+                     count+=1                
+         else:
+             trys -= 1
+             print("Life lost! Lives left:"+str(trys)+"\n")
                         
-        print("The word is:\n" + string_letters)
-        if(count == 5):
-            fireworks.start()
+         print("The word is:\n" + string_letters)
+         if(count == 5):
+             fireworks.start()
             
-    print("You lost!")
-    time.sleep(10)
+     print("You lost!")
+     time.sleep(10)
                 
 def game():
-    with open("words.txt") as w:
+    with ("words.txt") as w:
         words = w.read()
-
+    
     op = random.randint(0,5757)
     guess = {}
-    
+
     for i in range((op*6),(op*6)+5):
         guess[i] = words[i];
-        
+    
     print("Welcome to Wordle!")
     rule = input("Would you like to read the rules of the game?")
     if rule.capitalize() == "Yes":
@@ -61,9 +62,9 @@ def game():
 def rules_of_game(mystery_word,op):
     print("Guess any letter in the English Alphabet to fill up the word. You have 10 trys before you lose the game. Good luck!")
     start_game(mystery_word,op)
-
+        
 while 1:
-    os.system('clear')
+    os.system('clear')  
     start = input("Would you like to play wordle?\n")
 
     if start.capitalize() == "Yes":
