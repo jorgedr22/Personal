7654a1faed89795d8265622cbc230798f4e58633
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2023-04-12 01:04:23 -0700

    Update TopLevel.v

--- lab6/TopLevel.v
+++ lab6/TopLevel.v
@@ -6,6 +6,8 @@ module Lab6_toplevel (CLK100MHZ, BTNU, LED, CA, CB, CC, CD, CE, CF, CG, AN);
     output [7:0] AN;  //enable each digit of the 8 digits
     //write your code to connect the modules as shown in Figure 1 (page 3)
     
+    wire [7:0] sum;
+    
     (* mark_debug = "true" *) wire [7:0] debug_Reg0;
     (* mark_debug = "true" *) wire [7:0] debug_Reg1;
     (* mark_debug = "true" *) wire [7:0] debug_Reg2;
@@ -23,10 +25,9 @@ module Lab6_toplevel (CLK100MHZ, BTNU, LED, CA, CB, CC, CD, CE, CF, CG, AN);
     (* mark_debug = "true" *) wire [7:0] debug_Reg14;
     (* mark_debug = "true" *) wire [7:0] debug_Reg15;
 
-
-    mux a2(LED, temp2, count, D);
-    ClkDiv a3(CLK100MHZ,0,ClkOut);
+    ClkDiv a1(CLK100MHZ,0,ClkOut);
+    Lab6 a2(1, sum, LED, ClkOut, BTNU); //add wire to output R_data while done = 0
+    mux a3(LED, temp2, sum, D);
     TwoDigitDisplay a4(CLK100MHZ,D,CA,CB,CC,CD,CE,CF,CG,AN);
-    Lab6 a5(1, count, LED, ClkOut, BTNU, temp2);
     
 endmodule
