002e53a116cc02ba540fa4d3fea37eccc59d8f74
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2023-04-23 11:06:25 -0700

    updated lab 6(100%) and started lab exam 3

---
+++ Lab_Practicals/Lab_exam3.v
@@ -0,0 +1,16 @@
+timescale 1ns/1ps
+
+module EX3 (go,done,min,sum,CLK,Rst);
+    input go, CLK, Rst;
+    output reg done;
+    output reg [8:0] min, sum;
+
+    
+
+
+
+
+
+
+
+endmodule
--- lab6/lab6.v
+++ lab6/lab6.v
@@ -2,8 +2,7 @@
 
 module Lab6(go, sum, data, done, Clk, Rst);
     input go, Clk , Rst;
-    output reg [7:0] sum;
-    output reg [7:0] data;
+    output reg [7:0] sum, data;
     output reg done;
     
     wire [7:0] R_data;
@@ -15,15 +14,20 @@ module Lab6(go, sum, data, done, Clk, Rst);
                    s3=3, 
                    s4=4, 
                    s5=5, 
-                   s6=6;
+                   s6=6,
+                   s_wait = 7,
+                   s_disp = 8,
+                   s_wait2 = 9,
+                   s_disp2 = 10,
+                   s_disp_done = 11;
 
     
     reg [3:0] state;
     reg [7:0] temp;
     reg R_en, W_en;
     reg [7:0] W_data;
-    integer i;
-
+    reg [4:0] i;
+    reg [13:0] k;
     
     RegFile16x8 a1(i, i, R_en, W_en, R_data, W_data, Clk, Rst,debug_Reg0,debug_Reg1,debug_Reg2,debug_Reg3,debug_Reg4,debug_Reg5,debug_Reg6,debug_Reg7,debug_Reg8,debug_Reg9,debug_Reg10,debug_Reg11,debug_Reg12,debug_Reg13,debug_Reg14,debug_Reg15);
 
@@ -52,7 +56,7 @@ module Lab6(go, sum, data, done, Clk, Rst);
                     state <= s2; 
                 end
                 else begin
-                    done <= 1;
+                    R_en <= 0;
                     state <= 6;
                 end
             end
@@ -73,7 +77,6 @@ module Lab6(go, sum, data, done, Clk, Rst);
             end            
             s4: begin
                 sum <= sum + (temp - 48);
-                data <= W_data;
                 state <= s5;
             end
             s5: begin
@@ -82,8 +85,48 @@ module Lab6(go, sum, data, done, Clk, Rst);
                 state <= s1;
             end            
             s6: begin
-                done <=0;
-                state <= s0;
+                i <= 0;
+                k <= 0;
+                state <= s_wait;
+            end
+            s_wait:begin
+                if (k < 10000) begin
+                    k <= k + 1;
+                    state <= s_wait;
+                end
+                else begin
+                    R_en <= 1;
+                    done <= 1;
+                    state <= s_disp; 
+                end
+            end
+            s_disp:begin
+                    k <= 0;
+                    data <= R_data;
+                    state <= s_wait2;
+            end
+            s_wait2:begin
+                if (k < 10000) begin
+                    k <= k + 1;
+                    state <= s_wait2;
+                end
+                else begin
+                    state <= s_disp2; 
+                end
+            end
+            s_disp2:begin
+                if (i<=15) begin
+                    k <= 0;
+                    i <= i + 1;
+                    data <= R_data;
+                    state <= s_wait2;
+                end
+                else begin
+                    state <= s_disp_done;
+                end
+            end
+            s_disp_done:begin
+            
             end                                
         endcase
     end
