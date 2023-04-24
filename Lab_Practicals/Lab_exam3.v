4a7d31dbf68782247d4aac153413abe1a19ee858
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2023-04-23 22:55:42 -0700

    Update Lab_exam3.v

--- Lab_Practicals/Lab_exam3.v
+++ Lab_Practicals/Lab_exam3.v
@@ -1,17 +1,19 @@
-timescale 1ns/1ps
+`timescale 1ns / 1ps
 
 module EX3 (go,done,min,sum,Clk,Rst);
     input go, Clk, Rst;
     output reg done;
-    output reg [8:0] min, sum;
+    output reg [7:0] min;
+    output reg [12:0]sum;
 
-    localparam [3:0] = s0=0,s1=1,s2=2,s3=3,s4=4,s5=5,s6=6;
+    localparam [3:0] s0=0,s1=1,s2=2,s3=3,s4=4,s5=5,s6=6;
 
     reg [3:0] state;
     reg [7:0] temp;
     reg W_en,R_en;
-    reg [4:0] i;
-    reg [7:0] W_Data,R_Data;
+    reg [3:0] i;
+    reg [7:0] W_Data;
+    wire [7:0] R_Data;
 
     Register16_8 A1(i, i, R_en, W_en, R_Data, W_Data, Clk, Rst);
 
@@ -19,58 +21,82 @@ module EX3 (go,done,min,sum,Clk,Rst);
         if(Rst)begin
             state <= s0;
         end
-        else 
+        else begin
             case(state)
-                s0:
-                    if (go) state <= s1;
-                    else state <= s0;
-                s1:
+                s0:begin
+                    done <= 0;
+                    sum <= 0;
+                    min <= 255;
+                    i <= 0;
                     W_en <= 0;
                     R_en <= 0;
-                    if (i<16) state <= s2;
+                    if (go) state <= s1;
+                    else state <= s0;
+                end
+                s1:begin
+                    if (i<16) begin
+                        R_en <= 1;
+                        state <= s2;
+                    end
                     else state <= s6;
-                s2:
-                    R_en <= 1;
-                    if (temp<min) state <= s3;
-                    else if (temp > 128) || (temp == 128) state <= s4;
-                    else state <= s5;
-                s3:
-                    if(temp > 128) || (temp == 128) state <= s4;
-                    else state <= s5;
-                s4:
+                end    
+                s2:begin
+                    temp <= R_Data;
+                    if (temp < min) begin
+                    state <= s3;
+                    end
+                    else if (temp >= 128) begin
                     W_en <= 1;
+                    state <= s4;
+                    end
+                    else state <= s5;
+                end
+                s3:begin
+                    if (temp >= 128) begin
+                    W_en <= 1;
+                    min <= temp;
+                    state <= s4;
+                    end
+                    else state <= s5;
+                end
+                s4:begin
+                    W_Data <= (temp - 128);
                     state <= s5;
-                s5:
+                end
+                s5:begin
+                    sum <= (sum + temp);
+                    i <= i + 1;
                     state <= s1;
+                end
                 s6:
                     done <= 1;
+                default:
+                    state <= s0;
             endcase
+       end
     end
 
-    always @(*) begin
-        case (state)
-            s0:
-                done <= 0;
-                sum <= 0;
-                min <= 255
-                i <= 0;
-            s2:
-                temp = R_Data;
-            s3:
-                min = temp;
-            s4:
-                R_Data = temp - 128;
-            s5:
-                sum = sum + temp;
-                i = i + 1; 
-            s6:
-                done <= 1;
-
-        endcase
-    end
-
-
-
-
+//    always @* begin
+//        case (state)
+//            s0:begin
+//                done <= 0;
+//                sum <= 0;
+//                min <= 255;
+//                i <= 0;
+//            end
+//            s2:
+//                temp <= R_Data;
+//            s3:
+//                min <= temp;
+//            s4:
+//                W_Data <= (temp - 128);
+//            s5:begin
+//                sum <= (sum + temp);
+//                i <= i + 1; 
+//            end
+//            s6:
+//                done <= 1;
+//        endcase
+//    end
 
 endmodule
