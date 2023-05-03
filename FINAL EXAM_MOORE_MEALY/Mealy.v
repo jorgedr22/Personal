ad459957d1eabceb66417534d003b3d218b9dfe0
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2023-05-03 12:14:03 -0700

    .

--- FINAL EXAM_MOORE_MEALY/Mealy.v
+++ FINAL EXAM_MOORE_MEALY/Mealy.v
@@ -2,7 +2,7 @@ module Moore (b,x,Rst,Clk);
     input b,Rst,Clk;
     output reg x;
 
-    parameter [2:0] s0=0,s1=1,s2=2,s3=3;
+    parameter [2:0] s0=0,s1=1,s2=2;
     reg [2:0] state,n_state;
 
     always @(posedge CLK) 
@@ -24,14 +24,13 @@ module Moore (b,x,Rst,Clk);
                 if (~b) n_state <= s2;
                 else n_state <= s1;
             end
-            s2:begin
-                if (b) n_state <= s3;
-                else n_state <= s0;
-            end
-            s3:begin
-                x <= 1; //when state s3 is reached x = 1
-                if (b) n_state <= s1;
-                else n_state <= s0;
+            s2:begin // up to this point sequence 10 has been detected
+                if (b) // if b is 1, sequence is now 101, output is 1 and goes back to s0
+                begin
+                    n_state <= s0;
+                    x <= 1;
+                end
+                else n_state <= s0; // else output is 0 and goes back to s0
             end
         endcase
     end
